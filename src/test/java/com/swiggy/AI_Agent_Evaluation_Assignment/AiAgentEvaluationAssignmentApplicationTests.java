abe6e9a53db5128e50d0c8333d3a6994bc2735a8
avnish-3199 <avnish3199@gmail.com>
2026-03-18 23:19:09 +0530

    First commit

---
+++ src/test/java/com/swiggy/AI_Agent_Evaluation_Assignment/AiAgentEvaluationAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiggy.AI_Agent_Evaluation_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentEvaluationAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
